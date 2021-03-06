35a0c3205f0e03964dc3f618cd2ace233403fbff
Nikhil Raj <darklacerta7817@gmail.com>
2021-03-06 18:43:59 +0530

    Minor Changes

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ from discord.ext import commands
 import requests
 import json
 from dotenv import load_dotenv
+import imgToTxt
+from imgToTxt import response
 
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
-mathpixurl = 'https://api.mathpix.com/v3/latex'  
+TOKEN = os.getenv('DISCORD_TOKEN')  
 
 bot = commands.Bot(command_prefix="'")
 
@@ -28,23 +28,9 @@ async def getproblem(ctx,qimageurl=""):
     "data_options":{
         "include_latex": true
     }'''
-    response = requests.post(
-            mathpixurl,
-            data=json.dumps(query),
-            headers={
-                "app_id": os.getenv('APP_ID'),
-                "app_key": os.getenv('APP_KEY'),
-                "Content-Type": "application/json"
-            }
-        )
+    print(response)
     latex = json.dumps(json.loads(response.data))   
     print(latex)
-
-
-
-    
-
-        
 
 
 bot.run(TOKEN)
