6e613f68793506c2307f0e4738fd6c38009b4b6a
Nikhil Raj <darklacerta7817@gmail.com>
2021-03-06 11:23:36 +0530

    Bot takes input as image file or url

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import os
 from discord.ext import commands
-
+import requests
 from dotenv import load_dotenv
+from PIL import Image
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,8 +14,14 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
 @bot.command(name='mis',help='help dialogue')
-async def getproblem(ctx):
+async def getproblem(ctx,qimageurl=""):
     await ctx.send('hello')
+    if qimageurl == "" and ctx.message.attachments:
+        qimageurl = ctx.message.attachments[0].url
+    await ctx.send(qimageurl)
+    
+
+          
 
 
 
